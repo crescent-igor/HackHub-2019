e7320602d2317fe9f74dd38e07113389b0a329d6
wingedkittykat <43650548+wingedkittykat@users.noreply.github.com>
2019-03-30 05:13:02 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -2,12 +2,19 @@ import socket
 import select
 
 BUFFER = 1024
-data_list = {'1':0,'2':0} #dict to check cound against id
-msg = {'1':'Go to loc 1', '2':'Go to loc 2'}
+data_list_count = {'1':[],'2':[]} #dict to check count against id
+data_list_cordinates = {'1':[],'2':[]} 
+avg_dict = {'1':[],'2':[]} 
+avg1, avg2 = 0,0
+msg = {'1':"loc 1",'2':"loc 2"}
+
+def Average(lst): 
+    return sum(lst) / len(lst) 
 
 socket_list = []
 host = ''
-port = 12344
+port = 12345
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setblocking(0)
@@ -29,10 +36,21 @@ while socket_list:
             try:
                 data = socket.recv(BUFFER)
                 if data:
-                    d = data.decode('ascii');
+                    d = data.decode('ascii')
                     if(d[0]=='1' or d[0]=='2'):
-                        data_list[d[0]]= int(d[1:])
-                        print(data_list)
+                        # data_list[d[0]]= d[1:]
+                        recvData=d.split(':')
+                        if(len(data_list_count[d[0]])>20):
+                            data_list_count[d[0]].pop(0)
+                        data_list_count[d[0]].append(int(recvData[1]))
+                        # data_list_cordinates[d[0]].append(recvData[2])
+                        print(recvData)
+                        if d[0]=='1':
+                            avg1 = Average(data_list_count[d[0]])
+                        if d[0]=='2':
+                            avg2 = Average(data_list_count[d[0]])
+                        print(max(avg1,avg2))
+                                        
                     if(d[0]=='3'): 
                         keymax = max(data_list, key=data_list.get) 
                         socket.send(str(msg[keymax]).encode('ascii'))
@@ -45,4 +63,4 @@ while socket_list:
             except ConnectionResetError as e:
                 print(socket.getpeername(), 'has closed the connection!')
                 socket_list.remove(socket)
-                socket.close()
+socket.close()
